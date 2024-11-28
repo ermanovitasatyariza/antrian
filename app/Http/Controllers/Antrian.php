76df9d83f7c502bd4a6f11d08aebc9a7f9d9d9cc
ermanovitasatyariza <ermanovitasatyariza@gmail.com>
2024-11-28 15:15:35 +0700

    langkah awal

---
+++ app/Http/Controllers/Antrian.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Antrian extends Controller
+{
+    //
+    public function index(){
+        $data['antri_cs'] = 0;
+        $data['antri_teller1'] = 0;
+        $data['antri_teller2'] = 0;
+        $data['antri'] = "-";
+        return view('antrian'.$data);
+    }
+}
